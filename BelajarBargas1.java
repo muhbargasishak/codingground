da707608f867517a32f91aa660eeacbcd9f3d14f
muhbargasishak <31468805+muhbargasishak@users.noreply.github.com>
2017-09-03 00:08:51 +0700

    Add files via upload

---
+++ BelajarBargas1.java
@@ -0,0 +1,14 @@
+public class BelajarBargas1{
+
+     public static void main(String []args){
+        System.out.println("\nProgram Menampilkan Nama dan NIM (Java8)");
+        
+        // Menampilkan Nama
+        System.out.println("Nama : Muhammad Bargas ishak");
+        
+        // Menampilkan NIM
+        System.out.println("Nim  : "  +17220036+  "\n");
+    }
+}
+     
+
