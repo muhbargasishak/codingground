004e124a6f8740f70baaddc884e3b7d31f05f782
muhbargasishak <31468805+muhbargasishak@users.noreply.github.com>
2017-09-03 00:34:16 +0700

    Add files via upload

---
+++ BelajarBargas2.java
@@ -0,0 +1,23 @@
+public class BelajarBargas2 {
+    public static void main(String[] args) {
+        System.out.println("\nProgram Penghitungan Berat Badan (Java8)");
+        
+        // Menggunakan Tipe Data "double" untuk pecahan dan nama variable "berat"
+        double berat;
+        // Menentukan isi variable
+        berat = 40.2;
+        // Menampilkan isi variable pertama
+        System.out.println("Berat badan awal = " + berat);
+        
+        // Menentukan isi variable kedua ditambah variable pertama
+        berat = berat + 2;
+        // Menampilkan hasil penjumlahan variable kedua
+        System.out.println("Berat badan setelah makan = " + berat);
+        
+        // Menentukan isi variable ketiga dikurang variable kedua
+        berat = berat - 1;
+        // Menampilkan hasil pengurangan variable ketiga
+        System.out.println("Berat badan tanggal tua = " + berat + "\n");
+    }
+}
+   
